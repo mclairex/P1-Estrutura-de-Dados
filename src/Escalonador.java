e47a42ccca1908d0ad3631b9d2ca3784a7ec6c63
PedroJacquesDEV <jarodriguesj1@gmail.com>
2025-09-11 02:50:56 -0300

    criado método para executa um ciclo de CPU seguindo todas as regras da especificação

--- src/Escalonador.java
+++ src/Escalonador.java
@@ -4,7 +4,9 @@ public class Escalonador {
     private ListasDeProcessos mediaPrioridade;
     private ListasDeProcessos baixaPrioridade;
     private ListasDeProcessos bloqueados;
-    private ListasDeProcessos contagemAltaPrioridade;
+
+
+    private int contagemAltaPrioridade;
 
     // permite que o escalonador seja chamado em qualquer lugar do código
     public Escalonador(){
@@ -14,13 +16,47 @@ public class Escalonador {
         this.mediaPrioridade = new ListaDeProcessos();
         this.bloqueados = new ListaDeProcessos();
         this.contagemAltaPrioridade = 0;
+    }
+
+    public void executarCicloDeCPU() {
+        cicloAtual++;
+        System.out.println("\n=== CICLO " + cicloAtual + " ===");
+
+        // 1. Início do ciclo: desbloqueie o processo mais antigo da lista_bloqueados
+        desbloquearProcesso();
+
+        // 2. Mostrar estado atual (saída padronizada exigida)
+        mostrarEstadoListas();
+
+        // 3. Verificar regra de prevenção de inanição
+        if (contadorCiclosAltaPrioridade >= 5) {
+            System.out.println("🚨 REGRA ANTI-INANIÇÃO ATIVADA!");
+            executarProcessoMediaOuBaixa();
+            contadorCiclosAltaPrioridade = 0; // Contador é zerado após execução
+            return;
+        }
+
+    //Adicionar na lista baseado em sua prioridade
+
+    public void adicionarProcessos(Processo processo){
+        switch (processo.getPrioridade()){
+            case 1:
+                altaPrioridade.adicionarNoFinal(processo);
+                break;
+
+            case 2:
+                mediaPrioridade.adicionarNoFinal(processo);
+
+            case 3:
+                baixaPrioridade.adicionarNoFnial(processo);
+
+            default:
+                System.out.println("Essa prioridade é inválida " + processo.getNome());
+        }
+    }
+
 
 
     }
 
-    public void proximoCiclo(){
-        //implementar lógica...
-    }
-
-}
 
